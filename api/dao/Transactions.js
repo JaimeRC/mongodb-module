9bfd7fccbef46b3d0c6747556a2c0d2f618c31f5
JaimeRC <jaime.rc@icloud.com>
2020-04-06 09:09:54 +0200

    Update Transaction operation

--- api/dao/Transactions.js
+++ api/dao/Transactions.js
@@ -1,17 +1,14 @@
 const {env: {MONGO_DATABASE}} = process
-//const {User, Address} = require('./')
-let session, users, addresses
+
+let session, db
 
 module.exports = class Transactions {
     static async injectDB(conn) {
-        if (session)
-            return
+        if (session && db) return
 
         try {
             session = await conn.startSession()
-            users = await conn.db(MONGO_DATABASE).collection("users")
-            addresses = await conn.db(MONGO_DATABASE).collection("addresses")
-
+            db = await conn.db(MONGO_DATABASE)
         } catch (e) {
             console.error(`Unable to establish connection in Transactions: ${e}`)
         }
@@ -22,11 +19,11 @@ module.exports = class Transactions {
         try {
             session.startTransaction()
 
-            const newAddress = await addresses.insertOne(address, {session})
+            const newAddress = await db.collection("addresses").insertOne(address, {session})
 
             user.address = newAddress._id
 
-            const newUser = await users.insertOne(user, {session})
+            const newUser = await db.collection("users").insertOne(user, {session})
 
             await session.commitTransaction();
 
